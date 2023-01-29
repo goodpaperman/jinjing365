a6772720a927fba184941bc601f281f8ea6944fb
yunh <haihai107@126.com>
2023-01-29 16:16:23 +0800

    can query now

--- jinjing.sh
+++ jinjing.sh
@@ -4,40 +4,58 @@ function main()
 {
     # constant
     local stateurl="https://jjz.jtgl.beijing.gov.cn/pro//applyRecordController/stateList"
-    local agent="User-Agent: okhttp-okgo/jeasonlzy"
+    local agent="okhttp-okgo/jeasonlzy"
     local host="jjz.jtgl.beijing.gov.cn"
     local content="application/json;charset=utf-8"
     local lang="zh-CN,zh;q=0.8"
+    
     # read config
     local idcard=$(grep '^idcard=' config.ini | head -1 | awk -F'=' '{print $2}')
     local auth=$(grep '^authorization=' config.ini | head -1 | awk -F'=' '{print $2}')
     local source=$(grep '^source=' config.ini | head -1 | awk -F'=' '{print $2}')
     local localip=$(grep '^localip=' config.ini | head -1 | awk -F'=' '{print $2}')
+
     # query current status
-    local statereq=$(cat statereq.json | jq --arg sfzshm "${idcard}" --arg timestamp $(date "+%s") -c '{ v, sfzshm: $sfzshm, s-source, timestamp: $timestamp }')
-    echo "state req: ${statereq}"
+    # note: s-source should be quoted to prevent jq complain:
+    # jq: error: syntax error, unexpected '-', expecting '}' (Unix shell quoting issues?) at <top-level>, line 1:
+    local statereq=$(cat statereq.json | jq --arg sfzshm "${idcard}" --arg timestamp $(date "+%s") -c '{ v, sfzshm: $sfzshm, "s-source", timestamp: $timestamp }')
+    echo "state req: ${statereq}" 1>&2
     local stateheader=()
     stateheader[0]="ip:${localip}"
-    stateheader[1]="time:$(date '+%Y-%m-%d %H:%M:%S')"
-    stateheader[2]="Accept-Language:${lang}"
-    stateheader[3]="User-Agent:${agent}"
-    stateheader[4]="source:${source}"
-    stateheader[5]="authorization:${auth}"
-    stateheader[6]="Content-Type:${content}"
-    stateheader[7]="Content-Length:${#statereq}"
-    stateheader[8]="Host:${host}"
-    stateheader[9]="Connection:Keep-Alive"
-    stateheader[10]="Accept-Encoding:gzip"
+    stateheader[1]="Accept-Language:${lang}"
+    stateheader[2]="User-Agent:${agent}"
+    stateheader[3]="source:${source}"
+    stateheader[4]="authorization:${auth}"
+    stateheader[5]="Content-Type:${content}"
+    stateheader[6]="Content-Length:${#statereq}"
+    stateheader[7]="Host:${host}"
+    stateheader[8]="Connection:Keep-Alive"
+    stateheader[9]="Accept-Encoding:gzip"
+    # prevent whole time be truncated to only date
+    # add time alone here..
+    # stateheader[10]="time:$(date '+%Y-%m-%d %H:%M:%S')"
+    local time="time:$(date '+%Y-%m-%d %H:%M:%S')"
+    # size, what does it mean? seem to be optional..
     # stateheader[11]="size:459"
     local headers=""
-    for var in ${stateheader[@]}; 
+    for var in "${stateheader[@]}"; 
     do
         headers="${headers} -H ${var}"
     done
-    echo "state headers: ${headers}"
-    local resp=$(curl -s -H ${headers} -d "${statereq}" "${stateurl}")
-    echo "${resp}"
-    echo "${resp}" | jq  '.'
+    echo "state headers: ${headers} -H ${time}" 1>&2
+    local resp=$(curl -s ${headers} -H "${time}" -d "${statereq}" "${stateurl}")
+    echo "${resp}" | jq  '.'  1>&2
+    local ret=$(echo "${resp}" | jq -r '.code')
+    local msg=$(echo "${resp}" | jq -r '.msg')
+    if [ -z ${ret} -o "${ret}" = "null" -o ${ret} -ne 200 ]; then 
+        echo "query permits status failed, code: ${ret}, msg: ${msg}"
+        exit 1
+    fi
+
+    echo "query permits status ok: ${msg}"
+
+    # check if in effect
+
     # issue new permits request
 }
 
